7f4f14e3a4291ad0d81d3d87101c631ddc5dd0aa
geootrada <geootrada@yandex.ru>
2024-05-17 11:38:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = 5; 
+            int endNumber = 103;
+            int argument = 7;
+
+            for (int i = startNumber; i <= endNumber; i += argument)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
